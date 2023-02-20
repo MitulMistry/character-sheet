39e6844bdb210b00ecfb4a1162718c7d99d4a74a
Mitul Mistry <MitulMistryDev@gmail.com>
2023-02-20 17:05:02 -0600

    Implement Home page component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
+import { Button, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+import './Home.css';
+
 function Home() {
   return (
-    <div>
-      <p>Home</p>
+    <div className="home p-5 mb-4 bg-light rounded-3">    
+      <Container fluid="md">
+        <h1>Character Sheet</h1>
+        <p>Create character sheets for your table top RPGs.</p>
+        <p>Save them and update them as you play.</p>
+        <div className="buttons">
+          <LinkContainer to="/new">
+            <Button variant="primary">New Character</Button>
+          </LinkContainer>
+          <LinkContainer to="/load">
+            <Button variant="primary">Load Character</Button>
+          </LinkContainer>
+        </div>
+      </Container>
     </div>
   );
 }
