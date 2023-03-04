3fdc9678ddf0e39e2938e466288c6db40b7aecc9
Mitul Mistry <MitulMistryDev@gmail.com>
2023-03-04 16:14:23 -0600

    Configure Redux store to save and load state to browser's local storage

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,14 +3,31 @@ import counterReducer from '../features/counter/counterSlice';
 import characterFormReducer from '../components/CharacterForm/characterFormSlice';
 import charactersReducer from '../features/characters/charactersSlice';
 
+// Load the Redux state from the browser's local storage (if it exists)
+export const loadStateFromLocal = () => {
+  const preloadedState = localStorage.charSheetState ?
+    JSON.parse(localStorage.charSheetState) : {};
+  return preloadedState;
+};
+
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     characterForm: characterFormReducer,
     characters: charactersReducer
   },
+  preloadedState: loadStateFromLocal(),
 });
 
+// Save changes to Redux store to browser's local storage
+export const subscribeStoreToLocal = (store: StoreType) => {
+  store.subscribe(() => {
+    let state = store.getState();
+    localStorage.charSheetState = JSON.stringify(state);
+  });
+};
+
+export type StoreType = typeof store;
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import { subscribeStoreToLocal } from './app/store';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+
+// When there are changes to the Redux store, save
+// the state in the browser's local storage.
+subscribeStoreToLocal(store);
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
