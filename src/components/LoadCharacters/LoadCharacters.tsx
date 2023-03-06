c2f6ea72e1342c3eecf724d75d48a989b2fb8681
Mitul Mistry <MitulMistryDev@gmail.com>
2023-03-05 19:42:05 -0600

    Implement Masonry grid for saved characters and update styling

--- src/components/LoadCharacters/LoadCharacters.tsx
+++ src/components/LoadCharacters/LoadCharacters.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { selectCharacters } from '../../features/characters/charactersSlice';
 import { useAppSelector } from '../../app/hooks';
+import Masonry from 'react-masonry-css';
 import CharacterCard from './CharacterCard';
+import { Character } from '../../features/characters/charactersSlice';
 import { Link } from 'react-router-dom';
 import './LoadCharacters.css';
 
 function LoadCharacters() {
-  const characters = useAppSelector(selectCharacters);
-  
+  const characters: Character[] = useAppSelector(selectCharacters);
+  const breakpointColumnsObj = {
+    default: 3,
+    1199: 2,
+    767: 1
+  };
+
   return (
     <div className="load-characters">
       {characters.length === 0 && 
@@ -16,18 +23,23 @@ function LoadCharacters() {
           <p>Create a new character <Link to="/new">here</Link>.</p>
         </div>
       }
-      {characters.map((character, idx) => (
-        <CharacterCard
-          key = {`character-${idx}`}
-          id = {idx}
-          characterName = {character.characterName}
-          playerName = {character.playerName}
-          race = {character.race}
-          gender = {character.gender}
-          charClass = {character.charClass}
-          level = {character.level}
-        />
-      ))}
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="masonry-grid"
+        columnClassName="masonry-grid_column">      
+        {characters.map((character, idx) => (
+          <CharacterCard
+            key = {`character-${idx}`}
+            id = {idx}
+            characterName = {character.characterName}
+            playerName = {character.playerName}
+            race = {character.race}
+            gender = {character.gender}
+            charClass = {character.charClass}
+            level = {character.level}
+          />
+        ))}
+      </Masonry>
     </div>
   );
 }
